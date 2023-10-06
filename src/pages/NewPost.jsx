b53bf5066159bf4e5e164de012e236336ce7006c
Mamed <memmed.haqverdi02@gmail.com>
2023-10-06 11:54:27 +0400

    new

--- src/pages/NewPost.jsx
+++ src/pages/NewPost.jsx
@@ -8,7 +8,7 @@ const NewPost = ({
 
     return (
         <main className='NewPost'>
-            <button id='backbutton' onClick={() => navigate('/')}>&larr; Back</button>
+            <button id='backbutton' onClick={() => navigate('/posts')}>&larr; Back</button>
             <h2>New Post</h2>
             <form className='newPostForm' onSubmit={handleSubmit}>
                 <label htmlFor="postTitle">Title:</label>
